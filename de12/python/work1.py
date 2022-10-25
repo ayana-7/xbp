8c991fb9d575356308a52444d21d4b83b6e73a5f
ayana-7 <r202202108xi@jindai.jp>
2022-10-25 15:37:48 +0900

    20221025

---
+++ de12/python/work1.py
@@ -0,0 +1,10 @@
+#nameという変数に"aaa"を代入している
+name="aaa"
+#waistという変数に90を代入している
+waist=90
+#ageという変数に50を代入している
+age=50
+#heightという変数に175を代入している
+height=175
+
+print(name, "さんは腹囲", waist, "cmで年齢は",age, "才で身長は",height, "cmですね。")
